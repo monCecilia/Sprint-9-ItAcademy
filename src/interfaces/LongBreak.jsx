faee95c8a2056a98db7875c65aa2c46c9d6d007b
lachicho <mcmoneta@gmail.com>
2021-05-28 16:58:05 -0300

    agregue play a ls breaks

--- src/interfaces/LongBreak.jsx
+++ src/interfaces/LongBreak.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/logo_mate.png";
+import play from "../assets/icons/play.svg";
 function Portada() {
   return (
     
@@ -12,6 +13,9 @@ function Portada() {
           <Col className="d-flex flex-column justify-content-center align-items-center">
             <img src={logo} className="logo-break" />
             <h1 className="texto-break display-2">20:00</h1>
+            <div>
+            <img src={play} className="play" />
+          </div>
             {/* ACA VA CRONOMETRO CON 20 MINUTOS */}
           </Col>
         </Row>
--- src/interfaces/ShortBreak.jsx
+++ src/interfaces/ShortBreak.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import logo from '../assets/logo_mate.png'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/logo_mate.png";
+import play from "../assets/icons/play.svg";
 function Portada() {
-    return (
-        
-        <Container
-        fluid
-        className="background h-100 d-flex flex-column justify-content-center align-items-center align-content-center"
-      >
-                <Row className="altura-max d-flex flex-column justify-content-center align-items-center">
-                    <Col className ="d-flex flex-column justify-content-center align-items-center">
-                        <img src={logo} className="logo-break"/>
-                        <h1 className="texto-break display-2">05:00</h1>
-                        {/* ACA VA CRONOMETRO CON 5 MINUTOS */}
-                    </Col>
-                </Row>    
-            </Container>  
-    
-                 
-              
-    );
+  return (
+    <Container
+      fluid
+      className="background h-100 d-flex flex-column justify-content-center align-items-center align-content-center"
+    >
+      <Row className="altura-max d-flex flex-column justify-content-center align-items-center">
+        <Col className="d-flex flex-column justify-content-center align-items-center">
+          <img src={logo} className="logo-break" />
+          <h1 className="texto-break display-2">05:00</h1>
+          <div>
+            <img src={play} className="play" />
+          </div>
+          {/* ACA VA CRONOMETRO CON 5 MINUTOS */}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Portada;
-
