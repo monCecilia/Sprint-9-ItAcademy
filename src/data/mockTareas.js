c391ee5484ec0d274c1d4646fddf18c391f6fc08
Manuel Congosto <mc@y2kwebs.com>
2021-05-30 00:54:23 +0200

    integracion app

---
+++ src/data/mockTareas.js
@@ -0,0 +1,5 @@
+export const mockTareas = [
+  { titulo: "uno", comentario: "com1", mates: 1, matesHechos: 0 },
+  { titulo: "dos", comentario: "com2", mates: 2, matesHechos: 1 },
+  { titulo: "tres", comentario: "com3", mates: 3, matesHechos: 2 },
+];
